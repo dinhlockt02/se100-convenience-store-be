8d30d5ddbd5a620783e3c26138d859a32b99d7a0
Tran Dinh Loc <dinhlockt02@gmail.com>
2022-11-12 21:55:40 +0700

    add nginx service

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   const config = new DocumentBuilder()
     .setTitle('Convenience API')
@@ -16,6 +17,10 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document);
 
   app.useGlobalPipes(new ValidationPipe());
+
+  app.enableCors();
+
+  app.set('trust proxy', 1);
 
   await app.listen(3000);
 }
